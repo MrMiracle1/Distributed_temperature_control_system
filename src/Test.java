94398f2523bb427e84161e438fc37f060dc303a3
MrMiracle1 <1243702053@qq.com>
2021-05-29 21:58:31 +0800

    http

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+    public static void main(String[] args) {
+        try {
+            HttpRequester request = new HttpRequester();
+            HttpRespons hr = request.sendGet("http://localhost:8080");
+
+            System.out.println(hr.getUrlString());
+            System.out.println(hr.getProtocol());
+            System.out.println(hr.getHost());
+            System.out.println(hr.getPort());
+            System.out.println(hr.getContentEncoding());
+            System.out.println(hr.getMethod());
+
+            System.out.println(hr.getContent());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
